58f325f98b9777655acce96bb5b325b1a37104bf
kris-bo <kris-bo@users.noreply.github.com>
2024-06-19 13:13:20 +0200

    cleaned

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
-// // Assume you have a function to get the database from storage or API
-// let database = [];
-// fetch('1_us_urlsci_tech_output.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     database = data;
-//   });
-
-// // Listen for messages from the content script
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.action === 'getDatabase') {
-//     sendResponse({ database: database });
-//   }
-// });
-
 
 let database = [];
 fetch('db/1_us_urlsci_tech_output.json')
@@ -21,36 +6,9 @@ fetch('db/1_us_urlsci_tech_output.json')
     database = data;
   });
 
-// // Listen for messages from the content script
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.action === 'getDatabase') {
-//     sendResponse({ database: database });
-//   }
-// });
-
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     if (request.action === 'openTab') {
-//       console.log('openTab');
-//       chrome.tabs.create({ url: request.url });
-//       sendResponse({ message: 'Tab opened' });
-//     }
-//   });
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-if (request.action === 'openTab') {
-    chrome.tabs.create({ url: request.url }, function(tab) {
-    setTimeout(function() {
-        chrome.tabs.remove(tab.id);
-    }, 5000); // 5 seconds
-    });
-}
-});
 
 
 
-
-// background.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'getDatabase') {
 
@@ -81,43 +39,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     openNextTab();
   }
 });
-
-
-// chrome.runtime.sendMessage({ action: 'getDatabase' }, function(response) {
-//     const database = response.database;
-    
-//     // Iterate through the database entries
-//     database.forEach(entry => {
-//       if (window.location.href === entry.url + '/videos') {
-//         console.log('window.location.href   ');
-//         console.log(window.location.href);
-//         // Perform actions for this specific page
-//         console.log(`Found match for ${entry.name}!`);
-//         waitForPageLoad(entry.name);
-  
-//         chrome.runtime.sendMessage({ action: 'openTab', url: entry.url + '/videos' }, function(response) {
-//           console.log('openTab response', response);
-          
-//           // Get the ID of the newly opened tab
-//           chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-//             const newTabId = tabs[0].id;
-            
-//             // Wait for 50 seconds and then close the tab
-//             setTimeout(function() {
-//               chrome.tabs.remove(newTabId);
-//             }, 50000); // 50 seconds
-//           });
-//         });
-//       }
-//     });
-//   });
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     if (request.action === 'openTab') {
-//       chrome.tabs.create({ url: request.url }, function(tab) {
-//         setTimeout(function() {
-//           chrome.tabs.remove(tab.id);
-//         }, 5000); // 5 seconds
-//       });
-//     }
-//   });
